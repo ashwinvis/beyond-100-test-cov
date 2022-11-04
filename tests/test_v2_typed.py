3f9ea1cf2aaee501a44256a59b8510d53e354b86
Ashwin V. Mohanan <dev@fluid.quest>
2022-11-04 02:38:16 +0100

    Types in tests

--- tests/test_v2_typed.py
+++ tests/test_v2_typed.py
@@ -1,15 +1,17 @@
+from typing import List
+
 import pandas as pd
 import pytest
 
 import swedish_cities_v2_typed as sc
 
 
-def pandas_load(csv_file):
+def pandas_load(csv_file: str):
     df = pd.read_csv(csv_file, names=("city", "year")).set_index("city")
     return df.year.to_dict()
 
 
-def test_load(test_data):
+def test_load(test_data: str):
     data = sc.load_data(test_data)
     assert {"Gothenburg", "Malmö", "Skanör med Falsterbo"} == set(data.keys())
     assert data["Malmö"] == 1250
@@ -21,13 +23,13 @@ def test_should_be_filtered():
     assert sc.should_be_filtered("year >= 1500", 1500) is True
 
 
-def test_filter_data(test_data):
+def test_filter_data(test_data: str):
     data = pandas_load(test_data)
     cities = sc.filter_data(data, ["year < 1500"])
     assert all(data[city] < 1500 for city in cities)
 
 
-def test_print(test_data, capsys):
+def test_print(test_data: str, capsys):
     """Tests the print_data function"""
     data = pandas_load(test_data)
     sc.print_data(data)
@@ -35,8 +37,8 @@ def test_print(test_data, capsys):
     assert "was established in" in out
 
 
-@pytest.mark.parametrize("args", [("year >= 1500", "year < 1600"), ("year < 1300",)])
-def test_main(capsys, args):
+@pytest.mark.parametrize("args", [["year >= 1500", "year < 1600"], ["year < 1300",]])
+def test_main(capsys, args: List[str]):
     sc.main(args)
     out, err = capsys.readouterr()
     assert "was established in" in out
